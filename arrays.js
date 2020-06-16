1d125c2a3c0151c4695acbf1f6cf646679704304
Tristan Guest <tristanguest@gmail.com>
2020-06-16 17:07:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
   array = [element, ...array]
+  return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
