2355e2065fc17ed45639bfab76526c782fda850f
Tristan Guest <tristanguest@gmail.com>
2020-06-16 17:37:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,8 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
