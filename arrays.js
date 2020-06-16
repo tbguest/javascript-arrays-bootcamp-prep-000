c6452d109fc677b9cd04948f2b010b1e6dfa5f2f
Tristan Guest <tristanguest@gmail.com>
2020-06-16 17:10:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,8 +6,8 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = array.push(element)
-  return array
+  return array.push(element)
+  return 
 }
 
 console.log(destructivelyAddElementToBeginningOfArray(chocolateBars, 'twix'))
